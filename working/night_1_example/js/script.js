2db9c1c895408fae5def98077f3e0ae8721e2dce
foamyguy <foamyguy@gmail.com>
2022-09-13 17:17:22 -0500

    fix exercise and add night one working

---
+++ working/night_1_example/js/script.js
@@ -0,0 +1,5 @@
+console.log("hello from external js file");
+let hello_header = document.getElementById("hello_header");
+console.log(hello_header);
+
+hello_header.innerText = "Javascript is fun!";
